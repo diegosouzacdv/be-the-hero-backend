5b666308670a3721de2c59da2ecf905e302f8130
diego <diegoguitaibanez@gmail.com>
2020-04-06 09:47:59 -0300

    Listagem de prestador de servico

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -14,6 +15,8 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware); //só funcionam nas routas que vierem abaixo dessa linha;
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
